940c343699fe81472e6b87cf3dfe4302771af0ed
andrewipark <andrewipark@users.noreply.github.com>
2022-12-01 06:13:12 -0600

    1.2 fuck I was wrong

--- y2022/d1.py
+++ y2022/d1.py
@@ -1,13 +1,14 @@
-most = [0, None]
+import heapq
+heap = [[-1, None]] * 3
 with open('1.txt') as f:
 	current = [0, 0]
 	for line in f:
 		line = line.strip()
 		if not line:
-			if most[0] < current[0]:
-				most = current[:]  # ffs
+			heapq.heappushpop(heap, current[:])
 			current[1] += 1
 			current[0] = 0
 		else:
 			current[0] += int(line)
-print(most)
+print(heap)
+print(sum(x[0] for x in heap))
