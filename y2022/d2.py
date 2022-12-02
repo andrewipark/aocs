d6bd3bcc78e4470b3e207abd9ad3ea02291191ee
andrewipark <andrewipark@users.noreply.github.com>
2022-12-02 06:45:58 -0600

    fix names

--- y2022/d2.py
+++ y2022/d2.py
@@ -1,4 +1,3 @@
-import heapq
 from enum import IntEnum, auto
 
 class RPS(IntEnum):
@@ -6,15 +5,15 @@ class RPS(IntEnum):
 	PAPER = auto()
 	SCISSORS = auto()
 
-def which(opponent: RPS, you: RPS) -> int:
+def result(opponent: RPS, you: RPS) -> int:
 	"""-1, 0, or 1 for opponent, tie, you"""
 	return \
 		[[0, -1, 1],
 		 [1, 0, -1],
 		 [-1, 1, 0]][you][opponent]
-	# there's a better way to do this but I'm too lazy
+	# there's a better way to do this but I'm too lazy to handle wraparound
 
-def other_which(opponent: RPS, you_result: int) -> RPS:
+def which(opponent: RPS, you_result: int) -> RPS:
 	raw = opponent + you_result % 3 # b/c enum values ascend in the cycle of winning
 	if raw < 0:
 		raw += 3
@@ -30,20 +29,21 @@ def pb(l):
 	 # now it's the sign of the result we need
 	return (RPS(a - ord('A')), b - ord('Y'))
 
-def score(a: RPS, b: RPS):
+def score(opponent: RPS, you: RPS):
 	return (
-		int(b) + 1 # shape score
-		+ (which(a, b) + 1) * 3 # outcome score
+		you + 1 # shape score
+		+ (result(opponent, you) + 1) * 3 # outcome score
 	)
 
 def scoreb(opponent: RPS, you_result: int):
-	shape = other_which(opponent, you_result)
-	assert which(opponent, shape) == you_result, (opponent, you_result, shape)
+	shape = which(opponent, you_result)
+	assert result(opponent, shape) == you_result, (opponent, you_result, shape)
 	# print this for me, you dolt
 	return (
 		shape + 1 # shape score
 		+ (you_result + 1) * 3 # outcome score
 	)
+	# or: score(opponent, shape)
 
 with open('2.txt') as f:
 	print(sum((score(*p(x)) for x in f)))
