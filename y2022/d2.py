9578b206c88783fb50dda88a983402c85822f2c9
andrewipark <andrewipark@users.noreply.github.com>
2022-12-02 06:25:01 -0600

    2022-02a incl. dumb mistake

--- y2022/d2.py
+++ y2022/d2.py
@@ -6,17 +6,23 @@ class RPS(IntEnum):
 	PAPER = auto()
 	SCISSORS = auto()
 
-def which(a: RPS, b: RPS) -> int:
-	"""-1, 0, or 1 for a, tie, b"""
+def which(opponent: RPS, you: RPS) -> int:
+	"""-1, 0, or 1 for opponent, tie, you"""
 	return \
 		[[0, -1, 1],
 		 [1, 0, -1],
-		 [-1, 1, 0]][a][b]
+		 [-1, 1, 0]][you][opponent]
 	# there's a better way to do this but I'm too lazy
 
 def p(l):
 	a, b = (ord(x) for x in l.split())
 	return (RPS(a - ord('A')), RPS(b - ord('X')))
 
+def score(a: RPS, b: RPS):
+	return (
+		int(b) + 1 # shape score
+		+ (which(a, b) + 1) * 3 # outcome score
+	)
+
 with open('2.txt') as f:
-	print([p(x) for x in f][:5])
+	print(sum((score(*p(x)) for x in f)))
