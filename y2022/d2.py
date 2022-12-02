df62d9069e793a97b0666a5ff905131da87ea5a8
andrewipark <andrewipark@users.noreply.github.com>
2022-12-02 06:37:40 -0600

    2022-02b lmfao

--- y2022/d2.py
+++ y2022/d2.py
@@ -14,9 +14,21 @@ def which(opponent: RPS, you: RPS) -> int:
 		 [-1, 1, 0]][you][opponent]
 	# there's a better way to do this but I'm too lazy
 
+def other_which(opponent: RPS, you_result: int) -> RPS:
+	raw = opponent + you_result % 3 # b/c enum values ascend in the cycle of winning
+	if raw < 0:
+		raw += 3
+	raw %= 3
+	return RPS(raw)
+
 def p(l):
 	a, b = (ord(x) for x in l.split())
 	return (RPS(a - ord('A')), RPS(b - ord('X')))
+
+def pb(l):
+	a, b = (ord(x) for x in l.split())
+	 # now it's the sign of the result we need
+	return (RPS(a - ord('A')), b - ord('Y'))
 
 def score(a: RPS, b: RPS):
 	return (
@@ -24,5 +36,17 @@ def score(a: RPS, b: RPS):
 		+ (which(a, b) + 1) * 3 # outcome score
 	)
 
+def scoreb(opponent: RPS, you_result: int):
+	shape = other_which(opponent, you_result)
+	assert which(opponent, shape) == you_result, (opponent, you_result, shape)
+	# print this for me, you dolt
+	return (
+		shape + 1 # shape score
+		+ (you_result + 1) * 3 # outcome score
+	)
+
 with open('2.txt') as f:
 	print(sum((score(*p(x)) for x in f)))
+
+with open('2.txt') as f:
+	print(sum((scoreb(*pb(x)) for x in f)))
