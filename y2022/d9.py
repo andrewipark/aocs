91ec6def1e7faa6441af632daefdcad9a957cf86
andrewipark <andrewipark@users.noreply.github.com>
2022-12-09 09:23:22 -0600

    2022-09a

--- y2022/d9.py
+++ y2022/d9.py
@@ -18,17 +18,30 @@ def pf(f, pos_a, pos_b):
 def a(pos, d):
 	return pf(operator.add, pos, d.value)
 
+def s(x):
+	if x > 0:
+		return 1
+	if x < 0:
+		return -1
+	return 0
+
 def move(d, l, pos_h, pos_t, state):
 	for _ in range(l):
 		pos_h = a(pos_h, d)
 		delta = pf(lambda i, j: abs(i-j), pos_h, pos_t)
-		print(pos_h, pos_t, delta)
+		if max(delta) > 2:
+			assert False, (pos_h, pos_t, delta)
+		if max(delta) == 2:
+			# move_t = pf(lambda i, j: s(i-j), pos_h, pos_t)
+			# pos_t = pf(operator.add, pos_t, move)
+			pos_t = pf(lambda h, t: s(h-t) + t, pos_h, pos_t)
 		state[pos_t] = True
+
 	return pos_h, pos_t
 
 if __name__ == '__main__':
 	l = []
-	with open('i9s.txt') as f:
+	with open('i9.txt') as f:
 		l = f.read().split('\n')
 	l.pop()
 	l = [m.split(' ', 1) for m in l]
@@ -37,8 +50,7 @@ if __name__ == '__main__':
 	state = {}
 	pos_h = (0, 0)
 	pos_t = (0, 0)
-	for m in l[3:]:
-		print(m)
+	for m in l:
 		pos_h, pos_t = move(*m, pos_h, pos_t, state)
 
 	print(len(state))
