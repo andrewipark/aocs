f7e456e9c74aa207f9e5bf8a5bf1f64642d15d1f
andrewipark <andrewipark@users.noreply.github.com>
2022-12-10 05:48:57 -0600

    2022-10b: I know some Smash players who want that CRT

--- y2022/d10.py
+++ y2022/d10.py
@@ -4,6 +4,7 @@ def simul(l):
 	cycles = 1 # b/c we want values _during_ a given cycle not after
 	# 1-based indexing can die in a fire
 	li = 0
+	crt = [[False] * 40 for i in range(6)]
 
 	while li < len(l):
 		curr = l[li]
@@ -16,14 +17,22 @@ def simul(l):
 		for i in range(curr_cycles):
 			if (cycles - 20) % 40 == 0:
 				xs += (x * cycles)
+			c = cycles - 1
+			crt[c // 40][c % 40] = (abs(x - (c % 40)) <= 1)
 			cycles += 1
 
 		if curr != 'noop':
 			x += int(curr.split(' ')[1])
+			print(x)
 
 		li += 1
 
-	return xs
+	return xs, crt
+
+# TODO
+A = '\033[30m'
+B= '\033[90m'
+R= '\033[0m'
 
 if __name__ == '__main__':
 	l = []
@@ -31,4 +40,8 @@ if __name__ == '__main__':
 		l = f.read().split('\n')
 	l.pop()
 
-	print(simul(l))
+	xs, crt = simul(l)
+	print(xs)
+	for x in crt:
+		# TODO how do we detect whether we need 1 char (most ASCII fonts) or 2 char?
+		print(''.join(((B if y else A) + '██' + R for y in x)))
