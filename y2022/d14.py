c0a2724e896440bd3110123e1daf19331c4c7a6e
andrewipark <andrewipark@users.noreply.github.com>
2022-12-14 17:53:56 -0600

    2022-14a

--- y2022/d14.py
+++ y2022/d14.py
@@ -4,11 +4,33 @@ import numpy as np
 
 # ffs
 R= '\033[0m'
-cols = ['\033[30m', '\033[38;5;8m', '\033[38;140;8m']
+cols = ['\033[30m', '\033[38;5;8m', '\033[38;5;22m']
 
 def p0(grid):
 	for x in grid:
 		print(''.join((cols[y] + '█' for y in x)) + R)
+
+def simul_grain(board, la, lb):
+	while True:
+		while la < board.shape[0] and board[la, lb] == 0:
+			la += 1
+		if la >= board.shape[0]:
+			return None # fell off the bottom
+		if board[la, lb] != 0:
+			if lb <= 0:
+				return None # fell off left
+			if board[la, lb - 1] == 0:
+				lb -= 1
+				continue
+			if lb >= board.shape[1]:
+				return None # fell off right
+			if board[la, lb + 1] == 0:
+				lb += 1
+				continue
+			# all three spots are occupied
+			la -= 1
+			return la, lb
+		raise ValueError
 
 def main():
 	with open('i14.txt') as f:
@@ -37,8 +59,17 @@ def main():
 				pb, qb = min(mb, nb), max(mb, nb)
 				board[ma, pb:qb+1] = 1
 
-	p0(board)
+	c = 0
+	while True:
+		result = simul_grain(board, 0, 500 - min_b)
+		if not result:
+			break
+		c += 1
+		ga, gb = result
+		board[ga, gb] = 2
 
+	p0(board)
+	print(c)
 
 if __name__ == '__main__':
 	main()
