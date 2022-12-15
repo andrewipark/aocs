8a5e9635689df10616b87c95776a705d676a1e3e
andrewipark <andrewipark@users.noreply.github.com>
2022-12-15 06:12:50 -0600

    better soln

--- y2022/d15.py
+++ y2022/d15.py
@@ -18,7 +18,41 @@ def sensors_from_input(text):
 		s.append(tuple(map(int, (m[1], m[2], m[3], m[4]))))
 	return s
 
-def part_a(sensors, Y):
+def coalesce_closed_ranges(x):
+	subsumed = [False for _ in x]
+	new = []
+
+	# fix containing ranges
+	for i, r in enumerate(x):
+		if subsumed[i]:
+			continue
+		for j, s in enumerate(x[i+1:], i+1):
+			rd, rf = r
+			sd, sf = s
+			if rd <= sd and sf <= rf:
+				subsumed[j] = True
+				continue
+			if sd <= rd and rf <= sf:
+				subsumed[i] = True
+				break
+		if not subsumed[i]:
+			new.append(r) # survived
+
+	# fix half-overlapping ranges
+	new.sort()
+	i = 0
+	j = 1
+	while j < len(new):
+		p, n = new[i], new[j]
+		if p[1] >= n[0]:
+			new[i] = (p[0], n[1])
+			new[j] = None
+		else:
+			i = j
+		j += 1
+	return [n for n in new if n]
+
+def data(sensors, Y, constrain = False):
 	beacons = set(((s[2], s[3]) for s in sensors))
 	closed_ranges = []
 	for s in sensors:
@@ -29,8 +63,11 @@ def part_a(sensors, Y):
 			continue
 		closed_ranges.append((s[0] - sense_range, s[0] + sense_range))
 
+	return beacons, coalesce_closed_ranges(closed_ranges)
+
+def part_a(sensors, beacons, closed_ranges, Y):
 	# brute force
-	if True:
+	if False:
 		start_x = min((min(s[0], s[2]) for s in sensors)) * 6
 		end_x = max((max(s[0], s[2]) for s in sensors)) * 6
 		covered = 0
@@ -42,10 +79,13 @@ def part_a(sensors, Y):
 
 		return covered - len([b for b in beacons if b[1] == Y])
 
+	return sum((f - d + 1 for d, f in closed_ranges)) - len([b for b in beacons if b[1] == Y])
+
 def main():
 	with open('i15.txt') as f:
 		sensors = sensors_from_input(f.read())
-	print(part_a(sensors, 2000000))
+	Y = 2000000
+	print(part_a(sensors, *data(sensors, Y), Y))
 
 if __name__ == '__main__':
 	main()
