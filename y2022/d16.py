56e97b7a4ddfd587c84ab29bad9cd29f4b29e6c9
andrewipark <andrewipark@users.noreply.github.com>
2022-12-16 19:18:20 -0600

    somehow a broken pruning algorithm works?

--- y2022/d16.py
+++ y2022/d16.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict as ddd
 
 def l(text):
 	s = dict()
@@ -14,8 +15,8 @@ def contract(g):
 	to_remove = set((k for k, v in g.items() if v[0] == 0 and len(v[1]) == 2))
 	for x in to_remove:
 		a, b = g[x][1].keys()
-		g[a][1][b] = g[a][1][x] + g[x][1][b]
-		g[b][1][a] = g[b][1][x] + g[x][1][a]
+		g[a][1][b] = min(g[a][1].get(b, float('inf')), g[a][1][x] + g[x][1][b])
+		g[b][1][a] = min(g[b][1].get(a, float('inf')), g[b][1][x] + g[x][1][a])
 		del g[a][1][x], g[b][1][x], g[x]
 		assert g[a][1][b] == g[b][1][a], (g[a], g[b]) # symmetry
 	return g
@@ -23,13 +24,17 @@ def contract(g):
 def explore(g):
 	state = (30, 'AA', set(), 0)
 	stack = [state]
+	best_states = ddd(lambda: [-1] * 31)
 	best = 0
 	count = 0
 	while stack:
 		count += 1
 		time, where, ons, score = stack.pop()
+		ma = best_states[(where, frozenset(ons))]
+		for j in range(time + 1):
+			ma[j] = max(ma[j], score)
 
-		if g[where][0] > 0 and where not in ons and time > 1:
+		if g[where][0] > 0 and where not in ons and time > 0:
 			# turn the valve on
 			new_ons = ons.copy()
 			new_ons.add(where)
@@ -37,12 +42,13 @@ def explore(g):
 
 		stack.extend((
 			((time - dist), n, ons, score)
-			for n, dist in g[where][1].items() if time > dist
+			for n, dist in g[where][1].items()
+			if time > dist and (
+				n == 'AA' or max(best_states[(n, frozenset(ons))][:1]) < score
+			)
 		))
 
 		best = max(best, score)
-		if count % 10000 == 0:
-			print(len(stack), max(v[0] for v in stack))
 
 	print(f'explored {count} states')
 	return best
@@ -52,6 +58,7 @@ def main():
 		g = contract(l(f.read()))
 
 	print(g)
+	print(len(g))
 
 	print(explore(g))
 
