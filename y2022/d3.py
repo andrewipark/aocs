e772280295e46e6c9381cf650fbc4646885b0ed9
andrewipark <andrewipark@users.noreply.github.com>
2022-12-18 09:02:25 -0600

    2022-03: fix 'element from n=1 set' code + modernization

--- y2022/d3.py
+++ y2022/d3.py
@@ -9,7 +9,7 @@ def rucksack(l):
 def one_common_item(*i):
 	result = reduce(and_, (set(v) for v in i))
 	assert len(result) == 1, result # ffs
-	return list(result)[0]
+	return result.pop()
 
 def prio(x):
 	assert len(x) == 1, x
@@ -18,13 +18,21 @@ def prio(x):
 	else:
 		return ord(x) - ord('a') + 1
 
-with open('3.txt') as f:
-	# strip doesn't matter here b/c floor div. still chooses correctly
-	print(sum((prio(str(one_common_item(*rucksack(l)))) for l in f)))
+def a(lines):
+	# strip for trailing '\n' doesn't matter b/c (2n + 1) // 2 = n
+	return sum((prio(str(one_common_item(*rucksack(l)))) for l in lines))
 
-with open('3.txt') as f:
-	print(
+def b(lines):
+	return \
 		sum((prio(str(one_common_item(*[i.strip() for _k, i in g])))
-			 for _k, g in groupby(enumerate(f), lambda i: i[0] // 3)
+			 for _k, g in groupby(enumerate(lines), lambda i: i[0] // 3)
 		))
-	)
+
+def main():
+	with open('3.txt') as f:
+		lines = [l for l in f]
+		print(a(lines))
+		print(b(lines))
+
+if __name__ == '__main__':
+	main()
