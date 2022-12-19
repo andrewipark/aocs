d6433905a3a3cd98e8e122aa6fa74c9ba9f3e087
andrewipark <andrewipark@users.noreply.github.com>
2022-12-19 17:52:35 -0600

    2022-19 (both parts)
    - fix part a computation: i + (1 * v)
    - switch from tracking discrete minutes to tracking build actions
    since it's never beneficial to wait
    - don't produce more of a resource than we can consume
    
    As mentioned, there are other better algorithms,
    but this one appears functionally correct and barely works

--- y2022/d19.py
+++ y2022/d19.py
@@ -1,6 +1,7 @@
 import operator as op
 import re
 from functools import cache
+from math import prod
 from multiprocessing import Pool as mpool
 
 # resource tuples: (ore, clay, obsidian, geode)
@@ -12,13 +13,14 @@ def afford(have, cost):
 	assert len(have) == len(cost)
 	return all(map(op.ge, have, cost))
 
-def bill(have, cost):
-	assert len(have) == len(cost)
-	return tuple(map(op.sub, have, cost))
-
 def give(have, what):
 	assert len(have) == len(what)
 	return tuple(map(op.add, have, what))
+
+def turns_to_make(cost, have, rate):
+	if any((c != 0 and r == 0 for c, r in zip(cost, rate))):
+		return None
+	return max(max(((c - h + r - 1) // r for c, h, r in zip(cost, have, rate) if r != 0)), 0)
 
 def zeros_except_one(l, i):
 	a = [0] * l
@@ -46,51 +48,60 @@ def l(text):
 def max_geodes(t, costs):
 	builders = (1, 0, 0, 0)
 	resources = (0, 0, 0, 0)
-	max_ore_cost = tuple((max(v) for v in zip(*costs)))
-	print(max_ore_cost)
+	max_resource_cost = [max(z) for z in zip(*costs)]
+	max_resource_cost[GEODE] = float('inf')
 
-	# SLOW
 	@cache
 	def score(t, builders, resources):
-		if t <= 1:
-			#print(t, builders, resources)
-			return builders[GEODE] + resources[GEODE]
+		turns_to_builder = [turns_to_make(costs[resource], resources, builders) for resource in range(MAX_RESOURCES)]
 
-		# always correct because builders don't become ready until the next turn
-		resources_after = give(resources, builders)
 		results = [
 			score(
-				t - 1,
+				t - turns_to_builder[resource] - 1,
 				give(builders, zeros_except_one(MAX_RESOURCES, resource)),
-				bill(resources_after, costs[resource])
+				tuple((
+					cr - c + b * (turns_to_builder[resource] + 1)
+					for cr, c, b in zip(resources, costs[resource], builders)
+				))
 			)
 			for resource in range(MAX_RESOURCES)
-			if afford(resources, costs[resource])
-			and (resource == GEODE or builders[resource] < 5) # wrong way to make this run more quickly
+			if turns_to_builder[resource] is not None
+			and t > turns_to_builder[resource] + 1
+			# no point in more builders if we'd make more than we can consume
+			and builders[resource] < max_resource_cost[resource]
 		]
-		# always try to build the next available choice
-		if not results or not (
-			(builders[1] == 0 and afford(resources, costs[1]))
-			or (builders[2] == 0 and afford(resources, costs[2]))
-			or (builders[GEODE] == 0 and afford(resources, costs[GEODE]))
-		):
-			results.append(score(t - 1, builders, resources_after))
+		# SUBOPTIMAL per reddit, a better pruning strategy
+		# tracks the top-level maximum geodes so far and stops
+		# if the "current" branch cannot possibly beat that maximum
+		# other heuristics, e.g. always build a geode robot,
+		# might fail on pathological input but seem to be ok for AoC
+		if not results:
+			# won't be able to build anything else in time
+			return t * builders[GEODE] + resources[GEODE]
 		return max(results)
 
 	return score(t, builders, resources)
 
 def part_a_fn(b):
-	t = 8 # 24
 	return max_geodes(24, b)
 
+def part_b_fn(b):
+	return max_geodes(32, b)
+
 def main():
-	with open('i19s.txt') as f:
+	with open('i19.txt') as f:
 		b = l(f.read())
 
 	with mpool() as p:
-		result = list(p.map(part_a_fn, b))
-		print(result)
-		print(sum(((i + 1 * v) for i, v in enumerate(result))))
+		result_a = list(p.map(part_a_fn, b))
+		print(result_a)
+		print(sum(((i + 1) * v for i, v in enumerate(result_a))))
+
+		# naive multiprocessing doesn't help with this bad of a state space
+		# sharding some of the top level calls might help but seems complicated
+		result_b = list(p.map(part_b_fn, b[:3]))
+		print(result_b)
+		print(prod(result_b))
 
 if __name__ == '__main__':
 	main()
