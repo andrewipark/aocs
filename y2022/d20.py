071ace95e2262ace9006b06af0255e4de34bc23b
andrewipark <andrewipark@users.noreply.github.com>
2022-12-20 18:47:48 -0600

    2022-20b: permutations :(

--- y2022/d20.py
+++ y2022/d20.py
@@ -1,38 +1,65 @@
-def mix(s):
-	i = 0
+def mix(s, o):
+	assert len(s) == len(o)
 	processed = []
-	processed_expected = [c[0] for c in s]
-	while i < len(s):
-		curr = s[i]
-		v = curr[0]
-		assert curr[1] == 0
-		assert v == processed_expected[len(processed)], (i, v, processed_expected[len(processed)])
-		processed.append(v)
+	expected = [s[i] for i in o]
+	poses_o2n = {x: x for x in o}
+	for e in o:
+		i = poses_o2n[e]
+		v = s[i]
+		processed.append(s[i])
 
 		del s[i]
 		dest = (i + v) % len(s)
 		if (dest == 0 and v < 0):
 			dest = len(s) # wtf
-		# print(i, v, dest)
-		s[dest:dest] = [[v, curr[1] + 1]]
-		# print([c[0] for c in s])
+		# print(e, i, '->', dest)
+		s[dest:dest] = [v]
+		# print(s)
 
-		if dest <= i:
-			i += 1
-		while i < len(s) and s[i][1] > 0:
-			i += 1
-	assert processed == processed_expected, (processed, processed_expected)
-	assert all((lambda c: c[0] == 1 for c in s))
+		this_o2n = dict()
+		if dest < i:
+			this_o2n = {j: j + 1 for j in range(dest, i)}
+		elif dest > i:
+			this_o2n = {j + 1: j for j in range(i, dest)}
+		this_o2n[i] = dest
+
+		new_o2n = dict()
+		for k, v in poses_o2n.items():
+			if v in this_o2n:
+				new_o2n[k] = this_o2n[v]
+		# print(new_o2n)
+		for k, v in new_o2n.items():
+			poses_o2n[k] = v
+		# print(poses_o2n)
+
+	return poses_o2n
+
+def grove(s):
+	zi = s.index(0)
+	return s[(zi + 1000) % len(s)] + s[(zi + 2000) % len(s)] + s[(zi + 3000) % len(s)]
+
+def part_a(s):
+	mix(s, range(len(s)))
+	return grove(s)
+
+def part_b(s):
+	s = [811589153 * v for v in s]
+	o = range(len(s))
+	for i in range(10):
+		poses = mix(s, o)
+		o = [poses[i] for i in o]
+	return grove(s)
 
 def main():
 	with open('i20.txt') as f:
-		b = [[int(x), 0] for x in f.read().strip().split('\n')]
+		b = list(map(int, f.read().strip().split('\n')))
 
 	# A
-	mix(b)
-	# print([c[0] for c in b])
-	zi = b.index([0, 1])
-	print(b[(zi + 1000) % len(b)][0] + b[(zi + 2000) % len(b)][0] + b[(zi + 3000) % len(b)][0])
+	s_a = b[:]
+	print(part_a(s_a))
+
+	# B
+	print(part_b(b))
 
 if __name__ == '__main__':
 	main()
