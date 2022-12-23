f79410503886103e5f8c9a0463ecd2c5b2815781
andrewipark <andrewipark@users.noreply.github.com>
2022-12-23 10:36:00 -0600

    2022-23a

---
+++ y2022/d23.py
@@ -0,0 +1,81 @@
+from itertools import product
+import operator as op
+from math import prod
+
+# more grid drudgery; surely this is avoidable...
+
+def process(board_lines):
+	rows, cols = len(board_lines), max((len(l) for l in board_lines))
+	# sort points into proper category
+	mobs = {}
+	for p in product(range(rows), range(cols)):
+		r, c = p
+		match board_lines[r][c]:
+			case '#': mobs[p] = list('NSWE')
+	return mobs
+
+def propose(board, curr, order):
+	for d in product((-1, 0, 1), (-1, 0, 1)):
+		if d == (0, 0):
+			continue
+		if tuple(map(op.add, curr, d)) in board:
+			break
+	else:
+		return None # If no other Elves are in one of those eight positions, the Elf does not do anything
+	for d in order:
+		match d:
+			case 'N': deltas = [(-1, x) for x in (-1, 0, 1)]
+			case 'S': deltas = [(1, x) for x in (-1, 0, 1)]
+			case 'E': deltas = [(x, 1) for x in (-1, 0, 1)]
+			case 'W': deltas = [(x, -1) for x in (-1, 0, 1)]
+		if all((tuple(map(op.add, curr, d)) not in board for d in deltas)):
+			match d:
+				case 'N': return tuple(map(op.add, (-1, 0), curr))
+				case 'S': return tuple(map(op.add, (1, 0), curr))
+				case 'E': return tuple(map(op.add, (0, 1), curr))
+				case 'W': return tuple(map(op.add, (0, -1), curr))
+	return None
+
+def move(mobs, order):
+	# phase 1
+	curr_to_new = dict()
+	new_to_curr = dict()
+	collisions = set()
+
+	for curr in list(mobs.keys()):
+		new = propose(mobs, curr, order)
+		if not new:
+			continue
+		if new not in new_to_curr:
+			curr_to_new[curr] = new
+			new_to_curr[new] = curr
+		elif new not in collisions:
+			del curr_to_new[new_to_curr[new]]
+			del new_to_curr[new]
+			collisions.add(new) # phase 2
+
+	# ludacris
+	for curr, new in curr_to_new.items():
+		assert new not in mobs
+		mobs[new] = mobs[curr]
+		del mobs[curr]
+
+def part_a(mobs):
+	order = 'NSWE'
+	for i in range(10):
+		move(mobs, order)
+		order = order[1:] + order[0]
+	# TODO copied from 18 but d = 2 now
+	loc_min = tuple((min((l[d] for l in mobs)) for d in range(2)))
+	loc_max = tuple((max((l[d] for l in mobs)) for d in range(2)))
+	volume = prod(((x - n + 1) for x, n in zip(loc_max, loc_min)))
+	return volume - len(mobs)
+
+def main():
+	with open('i23.txt') as f:
+		mobs = process(f.read().strip().split('\n'))
+
+	print(part_a(mobs))
+
+if __name__ == '__main__':
+	main()
